264642262ebb87835742f41a252882cd57ce12ba
ziplock239 <michaelawazie72@gmail.com>
2026-04-29 12:17:00 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import { a4, a5 } from "./assets/worker-entry-BYvOMqif.js";
+import "node:events";
+import "node:async_hooks";
+import "node:stream/web";
+import "node:stream";
+export {
+  a4 as createServerEntry,
+  a5 as default
+};
